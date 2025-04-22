1359441daa4854844b0202ab0c262ac99c4e9691
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-04-22 21:13:00 +0545

    vite.config modified

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist', // default, but explicit
-  },
   server: {
-    host: true, // optional, helpful for local network testing
-  },
+    allowedHosts: ['learningsite-1.onrender.com', 'localhost'] // Add your host here
+  }
 });
