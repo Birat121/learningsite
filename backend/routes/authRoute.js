582eeafbc423eac15b1bf15c0455c497de99b0aa
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-04-25 12:15:24 +0545

    google login modified

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -30,7 +30,7 @@ authRouter.get("/google/callback",
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
