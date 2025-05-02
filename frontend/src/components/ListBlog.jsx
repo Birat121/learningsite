e3ec8e26a86d344e02b4302275e89a9318db103b
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-05-02 20:30:01 +0545

    some chnages

--- frontend/src/components/ListBlog.jsx
+++ frontend/src/components/ListBlog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axiosInstance from "../../api/axiosInstance";
+import axiosInstance from "../api/axiosInstance";
 import { toast } from "react-hot-toast";
 
 const ListBlogs = () => {
