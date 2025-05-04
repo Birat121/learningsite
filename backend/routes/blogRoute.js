60dd60f4ab7a005392cab9b9a75c668a862da946
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-05-04 11:18:34 +0545

    again some modification

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -9,11 +9,11 @@ import {
 
 const router = express.Router();
 
-router.post('blogs/', createBlog); // Create blog
-router.get('blogs/', getAllBlogs); // Get all blogs
-router.get('blogs/:slug', getBlogBySlug); // Get a single blog by slug
-router.put('blogs/:slug', updateBlog); // Update blog by slug
-router.delete('blogs/:slug', deleteBlog); // Delete blog by slug
+router.post('/blogs', createBlog); // Create blog
+router.get('/blogs', getAllBlogs); // Get all blogs
+router.get('/blogs/:slug', getBlogBySlug); // Get a single blog by slug
+router.put('/blogs/:slug', updateBlog); // Update blog by slug
+router.delete('/blogs/:slug', deleteBlog); // Delete blog by slug
 
 export default router;
 
