051fc49d1360072e62b0401442a926fcc96ff12e
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-05-06 19:03:55 +0545

    small changes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
@@ -8,11 +9,15 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const user = await User.findById(decoded.id);
+
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    req.user = user; // Now you’ll have full user object including `email`
     next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-};  
+};
 
 export default authMiddleware;
