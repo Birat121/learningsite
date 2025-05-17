c6d470c81862ae85c73513a772c0666cf137eaa5
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-05-17 16:34:34 +0545

    quiz added

--- backend/routes/quizRoute.js
+++ backend/routes/quizRoute.js
@@ -13,7 +13,7 @@ const quizRouter = express.Router();
 quizRouter.post("/quizzes", createQuiz);                  // Create quiz
 quizRouter.get("/quizzes", getAllQuizzes);                // Get all quizzes
 quizRouter.get("/quizzes/:id", getQuiz);                  // Get quiz by quiz ID
-quizRouter.get("/quizzes/:courseId", getQuizByCourseId);   // ✅ Get quiz by course ID
+quizRouter.get("/course/:courseId", getQuizByCourseId);   // ✅ Get quiz by course ID
 
 quizRouter.put("/quizzes/:quizId/questions/:questionIndex", updateQuestionInQuiz);
 quizRouter.delete("/quizzes/:quizId/questions/:questionIndex", deleteQuestionInQuiz);
