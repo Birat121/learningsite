ec8d2360f1c9d941cc81293ca072915bf8676daa
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-05-26 01:09:29 +0545

    small changes in smtp

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.example.com", // your SMTP server
+  host: "smtp.gmail.com", // your SMTP server
   port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
