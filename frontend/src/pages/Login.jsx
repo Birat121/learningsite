c1a7635374d649907cefef11f5f61f8326b61476
Birat Budhathoki <biratbudhathoki79@gmail.com>
2025-06-01 11:41:37 +0545

    small modification

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -80,7 +80,7 @@ const AuthPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center pt-28 sm:pt-32 px-4 sm:px-6 lg:px-8 ">
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center pt-28 sm:pt-32 px-4 sm:px-6 lg:px-8 mb-8 ">
 
       <div className="w-full max-w-md space-y-8">
         <div>
